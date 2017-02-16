3099ba23873cad2c47dc94af6567e889fa0eb934
cvsfeng <cvsfeng@163.com>
2017-02-16 14:00:05 +0800

    【ADD】  IPUtil

--- src/main/java/com/comb/commons/utils/ip/IPUtil.java
+++ src/main/java/com/comb/commons/utils/ip/IPUtil.java
@@ -1,6 +1,12 @@
 package com.comb.commons.utils.ip;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +17,45 @@ public class IPUtil {
 
     static final String REGEX = "((25[0-5]|2[0-4]\\d|1\\d{2}|0?[1-9]\\d|0?0?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|0?[1-9]\\d|0?0?\\d)";
     static final Pattern PATTERN = Pattern.compile(REGEX);
+
+    public static String LOCAL_IP = getLocalIp();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IPUtil.class);
+
+    private IPUtil() {
+    }
+
+    /**
+     * 获取本机ip地址
+     * 此方法为重量级的方法，不要频繁调用
+     * @return
+     */
+    public static String getLocalIp() {
+        if (LOCAL_IP != null) {
+            return LOCAL_IP;
+        }
+        try {
+            //根据网卡取本机配置的IP
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            String ip = null;
+            a:
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> ips = ni.getInetAddresses();
+                while (ips.hasMoreElements()) {
+                    InetAddress ipObj = ips.nextElement();
+                    if (ipObj.isSiteLocalAddress()) {
+                        ip = ipObj.getHostAddress();
+                        break a;
+                    }
+                }
+            }
+            return ip;
+        } catch (Exception e) {
+            LOGGER.error("", e);
+            return null;
+        }
+    }
 
     public  static String getClientIp(HttpServletRequest request) {
         //浏览器IP判断
