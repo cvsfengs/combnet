f16cae9d1eb2ed59cb9220913cb97c6d9bd715d5
cvsfengs <qer908052>
2017-02-18 11:53:22 +0800

    class demo

---
+++ src/test/java/ucmapi/generator/SimpleDemo.java
@@ -0,0 +1,23 @@
+package ucmapi.generator;
+
+/**
+ * Created by Administrator on 2017/2/13 0013.
+ */
+class SimpleDemo {
+    public static void main(String[] args) {
+
+        System.out.print((char)35);
+        System.out.print("-----------------------");
+    }
+
+
+
+}
+class SimpleDemo2 {
+    public static void main(String[] args) {
+        System.out.println("hello2");
+    }
+
+
+
+}
